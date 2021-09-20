f447c05fdf481b957b3a15346af8cbed372a898b
mchauke250 <mchauke250@gmail.com>
2021-09-20 21:53:13 +0200

    header and footer updated

---
+++ footer.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" href="assets/logo/tabLogo.jpg">
+<title>Soccer411</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+<link href="assets/css/theme.css" rel="stylesheet">
+<!-- Begin tracking codes here, including ShareThis/Analytics -->
+    
+<!-- End tracking codes here, including ShareThis/Analytics -->
+</head>
+			<footer class="footer">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="footer-widget">
+								<h5 class="title">Support</h5>
+								<ul>
+									<li><a target="_blank" href="FAQ.php">FAQ</a></li>
+									<li><a target="blank" href="errorReport.php">Report a Bug</a></li>
+									<li><a target="blank" href="tcapply.php">Terms & Conditions</a></li>
+									</ul>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="footer-widget">
+								<h5 class="title">Address</h5>
+								<ul>
+									<li><a href="tel:065-357-2171">Jali</a></li>
+									<li><a href="tel:079-444-9122">Rudolph</a></li>
+									<li><a href="contact.php">admin@soccer411.co.za</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="footer-widget textwidget">
+								<h5 class="title">Soccer411</h5>
+									<ul>
+										<li><a href="about.html">About Us</a></li>
+										<li><a target="_blank" href="#">Careers</li>
+										<li><a href="#">Leagues</a></li>
+										
+									</ul>
+								</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="footer-widget textwidget">
+								<div class="text-left footersocial">
+									<ul><h5>Connect with Us</h5>
+									<i class="fa fa-facebook"></i>
+									<i class="fa fa-twitter"></i>
+									<i class="fa fa-google"></i>
+									<i class="fa fa-pinterest"></i>
+									<i class="fa fa-github"></i></ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="copyright">
+						<p class="pull-left">
+							 <a target="_blank" href="#">Copyright © 2021 Soccer411</a>
+						</p>
+						<p class="pull-right">
+							<!-- Leave credit to author unless you own a commercial license: https://www.wowthemes.net/freebies-license/ -->
+							<a target="_blank" href="owners.php">Soccer411</a> - Rudolph Puane & Jali Mnisi
+						</p>
+					</div>
+				</div>
+				</footer>
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+		<script src="assets/js/masonry.pkgd.min.js"></script>
+		<script src="assets/js/theme.js"></script>
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" href="assets/logo/tabLogo.jpg">
+<title>Soccer411</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i" rel="stylesheet">
+<link href="assets/css/theme.css" rel="stylesheet">
+<!-- Begin tracking codes here, including ShareThis/Analytics -->
+    
+<!-- End tracking codes here, including ShareThis/Analytics -->
+</head>
+<body class="layout-default">
+<!-- Begin Menu Navigation
+================================================== -->
+<header class="navbar navbar-toggleable-md navbar-light bg-white fixed-top mediumnavigation">
+<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsWow" aria-controls="navbarsWow" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="container">
+	<!-- Begin Logo -->
+	<a class="navbar-brand" href="index.html">
+	<img src="assets/logo/homeLogo.jpg" style="width: 80%" alt="SOCCER411">
+	</a>
+	<!-- End Logo -->
+	<!-- Begin Menu -->
+	<div class="collapse navbar-collapse" id="navbarsWow">
+		<!-- Begin Menu -->
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+			<a class="nav-link" href="index.html">Home</a>
+			</li>
+			<li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Post</a>
+			<div class="dropdown-menu" aria-labelledby="dropdown01">
+				<a class="dropdown-item" href="single.html">Left Sidebar</a>
+				<a class="dropdown-item" href="single-sidebar-right.html">Right Sidebar</a>
+			</div>
+			</li>
+            <li class="nav-item">
+			<a class="nav-link" href="category.html">Category</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="about.html">About</a>
+			</li>
+			<li class="nav-item">
+			<a class="nav-link" href="contact.html">Contact</a>
+			</li>
+			<li class="nav-item">
+			<a target="_blank" class="nav-link highlight" href="login.php">Login</a>
+			</li>
+		</ul>
+		<!-- End Menu -->
+	</div>
+</div>
+</header>
+</body>
